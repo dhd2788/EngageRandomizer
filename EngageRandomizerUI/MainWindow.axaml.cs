5d553bdebd8dc93c6f2222864150b62209f6f4df
dhd2788 <dhd2788@rit.edu>
2023-04-08 00:22:37 -0400

    Added functional buttons to search for files and execute command prompt arguments. Cleaned up code and added documentation

--- EngageRandomizerUI/MainWindow.axaml.cs
+++ EngageRandomizerUI/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -8,22 +9,43 @@ namespace EngageRandomizerUI
 {
     public partial class MainWindow : Window
     {
+
+        /* important file locations */
+        private string engageAssetsLocation = "C:\\Users\\darad\\Desktop\\\"Switch Hacking\"\\FEE\\EngageAssetsModified";
+        private string engageXmlLocation = "C:\\Users\\darad\\Desktop\\\"Switch Hacking\"\\FEE\\EngageXML\\EngageXml.exe";
+
+        /**
+         * MainWindow: establish Avalonia debugging and initialize viewing screen
+         */
         public MainWindow()
         {
             Logger.TryGet(LogEventLevel.Fatal, LogArea.Control)?.Log(this, "Avalonia Infrastructure");
             System.Diagnostics.Debug.WriteLine("System Diagnostics Debug");
             InitializeComponent();
+            debugEnabled(true);
         }
 
-
-        public void btnRandomize_Click(object sender, RoutedEventArgs e)
+        /**
+         * debugEnabled: pre-establishes some variables for my own debugging purposes
+         * args:
+         *      enabled: whether or not to pre-establish variables
+         */
+        private void debugEnabled(bool enabled)
         {
-            var button = (Button)sender;
-            button.Content = "You clicked me!";
+            if (enabled)
+            {
+                engageAssetsLocation = "C:\\Users\\darad\\Desktop\\\"Switch Hacking\"\\FEE\\EngageAssetsModified";
+                engageXmlLocation = "C:\\Users\\darad\\Desktop\\\"Switch Hacking\"\\FEE\\EngageXML\\EngageXml.exe";
+            }
+}
 
-            string command1 = "cd C:\\Users\\darad\\Desktop\\\"Switch Hacking\"\\FEE\\EngageAssetsModified";
-            string command2 = "C:\\Users\\darad\\Desktop\\\"Switch Hacking\"\\FEE\\EngageXML\\EngageXml.exe calculator.xml";
-
+        /**
+         * executeCommands: executes a list of commands from the command prompt
+         * args:
+         *      commands    a list of string commands to execute from the command prompt
+         */
+        private void executeCommands(List<string> commands)
+        {
             Process cmd = new Process();
             cmd.StartInfo.FileName = "cmd.exe";
             cmd.StartInfo.RedirectStandardInput = true;
@@ -32,39 +54,83 @@ namespace EngageRandomizerUI
             cmd.StartInfo.UseShellExecute = false;
             cmd.Start();
 
-            cmd.StandardInput.WriteLine(command1);
-            cmd.StandardInput.Flush();
-
-            cmd.StandardInput.WriteLine(command2);
-            cmd.StandardInput.Flush();
+            foreach(string cmnd in commands)
+            {
+                cmd.StandardInput.WriteLine(cmnd);
+                cmd.StandardInput.Flush();
+            }
 
             cmd.StandardInput.Close();
             cmd.WaitForExit();
             System.Diagnostics.Debug.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
 
+        /**
+         * btnRandomize_Click: executes whenever the Randomize button is pressed. Performs randomization
+         * of all character base classes, starting inventories, and compartmentalizes them into .xml.bundle
+         * files using EngageXml.exe
+         */
+        private void btnRandomize_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            button.Content = "You clicked me!";
 
-            /*ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c" + command);
+            List<string> commands = new List<string>();
+            commands.Add("cd " + engageAssetsLocation);
+            commands.Add(engageXmlLocation + " calculator.xml");
 
-            procStartInfo.RedirectStandardOutput = true;
-            procStartInfo.UseShellExecute = false;
-            procStartInfo.CreateNoWindow = false;
-            
+            executeCommands(commands);
 
-            // wrap IDisposable into using (in order to release hProcess) 
-            using (Process process = new Process())
+        }
+
+        /**
+         * btnEngageXmlPath_Click: Executes whenever the EngageXmlPath button is pressed. Obtains the
+         * file location of EngageXml.exe and stores it in engageXmlLocation
+         */
+        private async void btnEngageXmlPath_Click(object sender, RoutedEventArgs e)
+        {
+            // open file window
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open EngageXml.exe";
+            ofd.Filters = new List<FileDialogFilter>() { new FileDialogFilter() { Name = "All files", Extensions = new List<string>() { "exe" } } };
+            ofd.AllowMultiple = false;
+            // wait for user
+            string[]? file = await ofd.ShowAsync(this);
+
+            // get file path
+            if (file == null)
             {
-                process.StartInfo = procStartInfo;
-                process.Start();
-
-                // Add this: wait until process does its work
-                process.WaitForExit();
-
-                // and only then read the result
-                string result = process.StandardOutput.ReadToEnd();
-                System.Diagnostics.Debug.WriteLine(result);
-            }*/
+                return;
+            }
+            else
+            {
+                engageXmlLocation = file[0];
+            }
 
 
+        }
+
+        /**
+ * btnEngageXmlPath_Click: Executes whenever the EngageXmlPath button is pressed. Obtains the
+ * file location of EngageXml.exe and stores it in engageXmlLocation
+ */
+        private async void btnEngageAssetsPath_Click(object sender, RoutedEventArgs e)
+        {
+            // open folder window
+            OpenFolderDialog ofd = new OpenFolderDialog();
+            ofd.Title = "Open Assets Folder";
+            // wait for user
+            string? file = await ofd.ShowAsync(this);
+
+            // get folder path
+            if (file == null)
+            {
+                return;
+            }
+            else
+            {
+                engageAssetsLocation = file;
+            }
         }
     }
 }
